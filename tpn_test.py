f0256ea989d38b0bb94205fc24a71d5bc7c41984
Dan Sanderson <contact@dansanderson.com>
2025-12-05 23:33:16 -0800

    Some tests for handle methods, but make_var_short isn't doing the right thing yet

--- tpn_test.py
+++ tpn_test.py
@@ -67,6 +67,25 @@ class TestLine(unittest.TestCase):
         self.assertIsNone(line.label)
         self.assertIsNone(line.line_number)
 
+    def test_handle(self):
+        state = TpnGenerator()
+        line = Line(statement=Statement("print ""hello"""))
+        self.assertEqual(line.handle(state, True), "100 print ""hello""")
+        self.assertEqual(state.cur_line, 110)
+
+    def test_handle_with_line_number(self):
+        state = TpnGenerator()
+        line = Line(line_number=200, statement=Statement("print ""hello"""))
+        self.assertEqual(line.handle(state, True), "200 print ""hello""")
+        self.assertEqual(state.cur_line, 210)
+
+    def test_handle_with_label(self):
+        state = TpnGenerator()
+        line = Line(label="label", statement=Statement("print ""hello"""))
+        self.assertEqual(line.handle(state, True), "100 print ""hello""")
+        self.assertEqual(state.labels["label"], 100)
+        self.assertEqual(state.cur_line, 110)
+
 
 class TestVarDeclare(unittest.TestCase):
     def test_parse_line(self):
@@ -103,6 +122,26 @@ class TestVarDeclare(unittest.TestCase):
         with self.assertRaises(TpnError):
             VarDeclare.parse_line("#declare varName foo")
 
+    def test_handle(self):
+        state = TpnGenerator()
+        var_declare = VarDeclare(name="varName")
+        var_declare.handle(state, False)
+        self.assertEqual(state.vars["varName"], "varname")
+
+    def test_handle_with_initializer(self):
+        state = TpnGenerator()
+        var_declare = VarDeclare(name="varName", init_short="123")
+        line = var_declare.handle(state, True)
+        self.assertEqual(state.vars["varName"], "varname")
+        self.assertEqual(line, "100 varname=123")
+
+    def test_handle_with_array_dim(self):
+        state = TpnGenerator()
+        var_declare = VarDeclare(name="varName", array_dim_short="10")
+        line = var_declare.handle(state, True)
+        self.assertEqual(state.vars["varName"], "varname")
+        self.assertEqual(line, "100 dim varname(10)")
+
 
 class TestDefine(unittest.TestCase):
     def test_parse_line(self):
@@ -124,6 +163,12 @@ class TestDefine(unittest.TestCase):
         self.assertEqual(define.name, "SYMBOL")
         self.assertEqual(define.value, "")
 
+    def test_handle(self):
+        state = TpnGenerator()
+        define = Define(name="SYMBOL", value="123")
+        define.handle(state, False)
+        self.assertEqual(state.defines["SYMBOL"], "123")
+
 
 class TestIfDef(unittest.TestCase):
     def test_parse_line_ifdef(self):
@@ -144,11 +189,36 @@ class TestIfDef(unittest.TestCase):
         with self.assertRaises(TpnError):
             IfDef.parse_line("#endif foo")
 
+    def test_handle_ifdef_defined(self):
+        state = TpnGenerator()
+        state.defines["SYMBOL"] = None
+        if_def = IfDef(def_name="SYMBOL", is_endif=False)
+        if_def.handle(state, False)
+        self.assertTrue(state.in_false_ifdef)
+
+    def test_handle_ifdef_undefined(self):
+        state = TpnGenerator()
+        if_def = IfDef(def_name="SYMBOL", is_endif=False)
+        if_def.handle(state, False)
+        self.assertFalse(state.in_false_ifdef)
+
+    def test_handle_endif(self):
+        state = TpnGenerator()
+        state.in_false_ifdef = True
+        if_def = IfDef(def_name=None, is_endif=True)
+        if_def.handle(state, False)
+        self.assertFalse(state.in_false_ifdef)
+
 
 class TestOutput(unittest.TestCase):
     def test_parse_line(self):
         output = Output.parse_line("#output foo.prg")
         self.assertIsNotNone(output)
+
+    def test_handle(self):
+        state = TpnGenerator()
+        output = Output()
+        self.assertIsNone(output.handle(state, False))
 
 
 class TestTpnGenerator(unittest.TestCase):
